8c00f7285dbcb60904da06e9557f4ab270a083b2
Jerry Kress <jerrykress.wsj@gmail.com>
2018-11-21 15:22:02 +0000

    Corrected mistakes. Changed font

--- app/src/main/java/spe/uoblibraryapp/Home.java
+++ app/src/main/java/spe/uoblibraryapp/Home.java
@@ -5,10 +5,25 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import spe.uoblibraryapp.api.wmsobjects.WMSBook;
+import spe.uoblibraryapp.api.wmsobjects.WMSLoan;
+import spe.uoblibraryapp.api.wmsobjects.WMSUserProfile;
+
 public class Home extends AppCompatActivity {
+
+//    ArrayList<WMSBook> loans = new ArrayList<>();
+//    WMSUserProfile user = new WMSUserProfile();
+//    loans = user.getLoans();
+//    for(loan:loans){
+//        loanList.add(loan.getBook().getTitle());
+//    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +32,11 @@ public class Home extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        MainListAdapter mainListAdapter = new MainListAdapter(getMealList());
+        MainListAdapter mainListAdapter = new MainListAdapter(getLoanList());
         recyclerView.setAdapter(mainListAdapter);
     }
 
-    public List<String> getMealList() {
+    public List<String> getLoanList() {
         List<String> loanList = new ArrayList<>();
         loanList.add("Book 1");
         loanList.add("Book 2");
