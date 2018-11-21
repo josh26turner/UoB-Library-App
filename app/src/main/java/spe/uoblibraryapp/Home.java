53de3f698c8656ee6e74d8a1af406cd1b7f5a18b
Denisi007 <dm17487@my.bristol.ac.uk>
2018-11-21 23:23:08 +0000

    Updated UI Branch

--- app/src/main/java/spe/uoblibraryapp/Home.java
+++ app/src/main/java/spe/uoblibraryapp/Home.java
@@ -1,5 +1,6 @@
 package spe.uoblibraryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent in = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(in);
     }
 }
