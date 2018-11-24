f66916416ef16e9c80dea49c73355e9cfb413a71
Denisi Murataj <denisimurataj@hotmail.co.uk>
2018-11-24 21:04:52 +0000

    integrated w/ NCIPController

--- app/src/main/java/spe/uoblibraryapp/LoansFragment.java
+++ app/src/main/java/spe/uoblibraryapp/LoansFragment.java
@@ -3,21 +3,18 @@ package spe.uoblibraryapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import spe.uoblibraryapp.api.WMSException;
 import spe.uoblibraryapp.api.ncip.WMSNCIPController;
-import spe.uoblibraryapp.api.wmsobjects.WMSBook;
 import spe.uoblibraryapp.api.wmsobjects.WMSLoan;
 import spe.uoblibraryapp.api.wmsobjects.WMSParseException;
+import spe.uoblibraryapp.api.wmsobjects.WMSRequest;
 import spe.uoblibraryapp.api.wmsobjects.WMSUserProfile;
 
 
@@ -30,13 +27,28 @@ public class LoansFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.fragment_loans, container, false);
 
         ListView mListView = (ListView) view.findViewById(R.id.listview);
+        WMSNCIPController wmsncipController = new WMSNCIPController();
+        ArrayList<LoanBookEntry> bookList = new ArrayList<>();
+
+        try {
+            WMSUserProfile userProfile = wmsncipController.getUserDetails("0");
+            for (WMSLoan loan : userProfile.getLoans()){
+                bookList.add(new LoanBookEntry(loan.getBook().getTitle(), loan.getBook().getPublisher(), "Loan"));
+            }
+        }
+        catch (WMSException e)
+        {
+            //TODO: Output some error.
+        }
+        catch (WMSParseException e){
+            //TODO: Output some error.
+        }
+
+
 
         LoanBookEntry bookOne = new LoanBookEntry("Book 1 Title", "Author 1", "Overdue");
         LoanBookEntry bookTwo = new LoanBookEntry("Book 2 Title", "Author 2", "Reservation");
         LoanBookEntry bookThree = new LoanBookEntry("Book 3 Title", "Author 3", "Loan");
-
-
-        ArrayList<LoanBookEntry> bookList = new ArrayList<>();
         bookList.add(bookOne);
         bookList.add(bookTwo);
         bookList.add(bookThree);
