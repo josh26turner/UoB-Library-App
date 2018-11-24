3ddd2088c718724d3a9869c1fac3178a0d3d04ce
Denisi007 <dm17487@my.bristol.ac.uk>
2018-11-24 00:21:14 +0000

    Added fragments to UI

---
+++ app/src/main/java/spe/uoblibraryapp/LoanHistoryFragment.java
@@ -0,0 +1,19 @@
+package spe.uoblibraryapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoanHistoryFragment extends android.support.v4.app.Fragment{
+
+    private static final String TAG = "Loan History Fragment";
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reservations, container, false);
+        return view;
+    }
+}
