37064392de517212ed714477c9ea1fd845e6ff3d
Denisi Murataj <denisimurataj@hotmail.co.uk>
2018-11-25 21:47:15 +0000

    renamed Home to ActivityHome

--- app/src/main/java/spe/uoblibraryapp/ActivityHome.java
+++ app/src/main/java/spe/uoblibraryapp/ActivityHome.java
@@ -12,7 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ActivityHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
     private CustomPagerAdapter mAdapter;
@@ -83,7 +83,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
         if (id == R.id.nav_scanBook) {
             //start activity here.
-            Intent NFCActivity = new Intent(getApplicationContext(), ScanNFCActivity.class);
+            Intent NFCActivity = new Intent(getApplicationContext(), ActivityScanNFC.class);
             startActivity(NFCActivity);
         } else if (id == R.id.nav_current_loans_reservations) {
             setViewPager("Loans");
