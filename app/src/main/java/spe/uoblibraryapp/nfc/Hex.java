5fee318386718790b34191cca3db44d8f2dcec3f
Josh Turner <joshaturner1@googlemail.com>
2018-11-28 12:27:48 +0000

    Hex readable update

--- app/src/main/java/spe/uoblibraryapp/nfc/Hex.java
+++ app/src/main/java/spe/uoblibraryapp/nfc/Hex.java
@@ -1,16 +1,25 @@
 package spe.uoblibraryapp.nfc;
 
 public class Hex {
+
+    /**
+     * This sets values for the ISO 15693 commands,
+     * as detailed in http://www.ti.com/lit/an/sloa141/sloa141.pdf
+     * It provides simple to use and easy reading commands for transmission to NfcV tags
+     */
+
+
     private static final byte GET_SYSTEM_INFO = (byte) 0x2B;
     private static final byte READ_MULTIPLE_BLOCKS = (byte) 0x23;
     private static final byte FLAGS = (byte) 0x00;
     private static final byte WRITE_AFI = (byte) 0x27;
-    static final byte AFI_CHECKED_IN = (byte) 0xDA;
-    static final byte AFI_CHECKED_OUT = (byte) 0xC2;
+    private static final byte AFI_CHECKED_OUT = (byte) 0xC2;
 
-    public static final byte [] SET_SECURITY_OFF = new byte[]{FLAGS, WRITE_AFI, AFI_CHECKED_OUT};
-    public static final byte [] SET_SECURITY_ON = new byte[]{FLAGS, WRITE_AFI, AFI_CHECKED_IN};
-    public static byte [] SYSTEM_INFO_COMMAND = new byte[]{FLAGS, GET_SYSTEM_INFO};
+    static final byte AFI_CHECKED_IN = (byte) 0xDA;
+
+    static final byte [] SET_SECURITY_OFF = new byte[]{FLAGS, WRITE_AFI, AFI_CHECKED_OUT};
+    static final byte [] SET_SECURITY_ON = new byte[]{FLAGS, WRITE_AFI, AFI_CHECKED_IN};
+    static byte [] SYSTEM_INFO_COMMAND = new byte[]{FLAGS, GET_SYSTEM_INFO};
 
     /**
      *
@@ -29,7 +38,7 @@ public class Hex {
      * @return - the number in bytes
      * @throws NumberFormatException - if not between -128 and 127
      */
-    private static byte toByte(int i) throws NumberFormatException{
+    private static byte toByte(int i) throws NumberFormatException {
         if ((i <= 127) && (i >= -128)) return (byte) i;
         else throw new NumberFormatException();
     }
