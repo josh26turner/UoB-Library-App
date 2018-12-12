125fd0c2de679c1c647575080a161d017a9f8513
Denisi Murataj <denisimurataj@hotmail.co.uk>
2018-12-12 11:41:20 +0000

    Added bounds to webview, finished sign on screen & added error checking

--- app/src/main/java/spe/uoblibraryapp/FragmentLoans.java
+++ app/src/main/java/spe/uoblibraryapp/FragmentLoans.java
@@ -23,6 +23,8 @@ import java.util.Queue;
 
 import spe.uoblibraryapp.api.WMSException;
 import spe.uoblibraryapp.api.ncip.WMSNCIPController;
+import spe.uoblibraryapp.api.wmsobjects.WMSBook;
+import spe.uoblibraryapp.api.wmsobjects.WMSHold;
 import spe.uoblibraryapp.api.wmsobjects.WMSLoan;
 import spe.uoblibraryapp.api.wmsobjects.WMSParseException;
 import spe.uoblibraryapp.api.wmsobjects.WMSUserProfile;
@@ -36,51 +38,59 @@ public class FragmentLoans extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_loans, container, false);
-
         ListView mListView = view.findViewById(R.id.listview);
         WMSNCIPController wmsncipController = new WMSNCIPController();
         ArrayList<LoanBookEntry> bookList = new ArrayList<>();
-        Queue<LoanBookEntry> bookQueue = new LinkedList<LoanBookEntry>();
+        Queue<LoanBookEntry> bookQueue = new LinkedList<>();
+        WMSUserProfile userProfile;
 
-
-        try {
-            WMSUserProfile userProfile = wmsncipController.getUserDetails("0");
-            Date date = new Date();
-
-            for (WMSLoan loan : userProfile.getLoans()){
-                if (loan.getDueDate().before(date)){
-                    //add to list-view -> item overdue.
-                    bookList.add(new LoanBookEntry(loan.getBook().getTitle(), loan.getBook().getAuthor(), BookStatus.OVERDUE));
-                }else{
-                    //push to queue for re-entry later.
-                    bookQueue.add(new LoanBookEntry(loan.getBook().getTitle(), loan.getBook().getAuthor(), BookStatus.LOAN));
+        /* Calls WMS a maximum of 3 times before fatal error occurs */
+        int exceptionCount = 0;
+        while (true) {
+            if (exceptionCount<3) {
+                try {
+                    userProfile = wmsncipController.getUserDetails("0");
+                    break;
+                } catch (WMSException e) {
+                    Toast.makeText(getActivity(), "ERROR: Cannot get user details, trying again. ", Toast.LENGTH_SHORT).show();
+                    exceptionCount++;
+                } catch (WMSParseException e) {
+                    Toast.makeText(getActivity(), "ERROR: Cannot get user details, trying again. ", Toast.LENGTH_SHORT).show();
+                    exceptionCount++;
                 }
             }
-            //add reservations.
-            bookList.add(new LoanBookEntry("Test Reservation 1", "Test Author 1", BookStatus.RESERVATION));
-            bookList.add(new LoanBookEntry("Test Reservation 2", "Test Author 2", BookStatus.RESERVATION));
-            bookList.add(new LoanBookEntry("Test Reservation 3", "Test Author 3", BookStatus.RESERVATION));
+            else Toast.makeText(getActivity(), "FATAL ERROR OCCURRED - Try again later.", Toast.LENGTH_LONG);
+        }
 
-            //add items from queue sorted by date taken out.
-            for (LoanBookEntry entry : bookQueue){
-                bookList.add(entry);
+        /* Overdue here */
+        Date date = new Date();
+        for (WMSLoan loan : userProfile.getLoans()) {
+            if (loan.getDueDate().before(date)) {
+                //add to list-view -> item overdue.
+                bookList.add(new LoanBookEntry(loan.getBook().getTitle(), loan.getBook().getAuthor(), BookStatus.OVERDUE));
+            } else {
+                //push to queue for re-entry later.
+                bookQueue.add(new LoanBookEntry(loan.getBook().getTitle(), loan.getBook().getAuthor(), BookStatus.LOAN));
             }
         }
-        catch (WMSException e)
-        {
-            //TODO: Output some error.
-        }
-        catch (WMSParseException e){
-            //TODO: Output some error.
-        }
+
+        /* Reservations here */
+        ArrayList<WMSHold> reserveQueueList = new ArrayList<>();
+        for (WMSHold hold : reserveQueueList)
+            bookList.add(new LoanBookEntry(hold.getBook().getTitle(), hold.getBook().getAuthor(), BookStatus.RESERVATION));
+        /* Sample Reservations */
+        bookList.add(new LoanBookEntry("Test Reservation 1", "Test Author 1", BookStatus.RESERVATION));
+        bookList.add(new LoanBookEntry("Test Reservation 2", "Test Author 2", BookStatus.RESERVATION));
+        bookList.add(new LoanBookEntry("Test Reservation 3", "Test Author 3", BookStatus.RESERVATION));
+
+        /*Loans added here */
+        for (LoanBookEntry entry : bookQueue)
+            bookList.add(entry);
 
         LoanBookListAdapter adapter = new LoanBookListAdapter(getContext(), R.layout.adapter_view_layout, bookList);
         mListView.setAdapter(adapter);
         ListView listview = view.findViewById(R.id.listview);
         listview.setDescendantFocusability(ListView.FOCUS_BLOCK_DESCENDANTS);
-
-
-
         return view;
     }
 }
