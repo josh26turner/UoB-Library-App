ec50ff1d110127bed005b98b4dee0048117c084a
Riley Evans <riley@rileyevans.co.uk>
2019-01-15 15:02:48 +0000

    Renamed spashscreen layout

--- app/src/main/java/spe/uoblibraryapp/SplashScreen.java
+++ app/src/main/java/spe/uoblibraryapp/SplashScreen.java
@@ -25,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.activity_splash_screen);
         StartAnimations();
     }
 
