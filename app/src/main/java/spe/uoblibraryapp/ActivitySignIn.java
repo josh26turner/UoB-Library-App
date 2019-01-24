bb34d54cb2532c017b3868be7a805ad0322457ce
Jerry Kress <jerrykress.wsj@gmail.com>
2019-01-24 13:59:20 +0000

    Restyled SSO pages for mobile view

--- app/src/main/java/spe/uoblibraryapp/ActivitySignIn.java
+++ app/src/main/java/spe/uoblibraryapp/ActivitySignIn.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
@@ -11,6 +12,8 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import java.io.InputStream;
 
 import stanford.androidlib.SimpleActivity;
 
@@ -36,6 +39,7 @@ public class ActivitySignIn extends SimpleActivity {
         mywebview.getSettings().setJavaScriptEnabled(true);
         mywebview.getSettings().setLoadWithOverviewMode(true);
         mywebview.getSettings().setUseWideViewPort(true);
+        mywebview.getSettings().setSupportZoom(true);
         mywebview.setInitialScale(1);
         mywebview.loadUrl("https://authn.sd00.worldcat.org/oauth2/authorizeCode?client_id=LRQvSrRL1pjZCy8R0AyQpL45QtYvJs6SpjKSF2EmqzmVc0mpIhE85ahM2m4XbByK9qMhl9IcX8fOeOet&authenticatingInstitutionId=132607&contextInstitutionId=132607&redirect_uri=uoblibrary%3A%2F%2Fauthenticate&response_type=token&scope=WMS_NCIP%20refresh_token");
         mywebview.setWebViewClient(new WebViewClient() {
@@ -63,8 +67,20 @@ public class ActivitySignIn extends SimpleActivity {
                     }
                 }
             }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+
+                // Inject CSS when page is done loading
+                injectCSS(view);
+
+                super.onPageFinished(view, url);
+            }
+
         });
     }
+
 
     @Override
     public void onBackPressed() {
@@ -75,11 +91,11 @@ public class ActivitySignIn extends SimpleActivity {
         // TODO could we change back button to work with webview?
     }
 
-    private boolean isAuthorisationDenied(String s){
+    private boolean isAuthorisationDenied(String s) {
         return s.contains("uoblibrary://authenticate#error");
     }
 
-    private void processAuthorisationString(String s){
+    private void processAuthorisationString(String s) {
 
         // TODO: Could this be a loop? store is a hash table, that way its future proof if they change the order of url arguments
         int startOfToken = s.indexOf("access_token=") + 13;
@@ -99,7 +115,7 @@ public class ActivitySignIn extends SimpleActivity {
         String refreshToken = s.substring(startOfRefreshToken, endOfRefreshToken);
 
         int startOfRefreshTokenExpires_at = s.indexOf("&refresh_token_expires_at=") + 26;
-        int endOfRefreshTokenExpires_at = s.length()-1;
+        int endOfRefreshTokenExpires_at = s.length() - 1;
         String refreshTokenExpiry = s.substring(startOfRefreshTokenExpires_at, endOfRefreshTokenExpires_at);
 
         //------put in sharedPreferences
@@ -111,9 +127,28 @@ public class ActivitySignIn extends SimpleActivity {
         pref.edit().putString("refreshTokenExpiry", refreshTokenExpiry).apply();
     }
 
-    public static void clearCookies(Context context)
-    {
+    public static void clearCookies(Context context) {
         CookieManager.getInstance().removeAllCookies(null);
         CookieManager.getInstance().flush();
+    }
+
+    private void injectCSS(WebView mywebview){
+        try {
+            InputStream inputStream = getAssets().open("black.css");
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            inputStream.close();
+            String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+            mywebview.loadUrl("javascript:(function() {" +
+                    "var parent = document.getElementsByTagName('head').item(0);" +
+                    "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    // Tell the browser to BASE64-decode the string into your script !!!
+                    "style.innerHTML = window.atob('" + encoded + "');" +
+                    "parent.appendChild(style)" +
+                    "})()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
