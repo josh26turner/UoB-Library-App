040911cd0446d08715512c2782409805f8fb9695
Denisi Murataj <denisimurataj@hotmail.co.uk>
2019-02-19 20:03:09 +0000

    Updated for Codacy Requirements

--- app/src/main/java/spe/uoblibraryapp/ActivitySignIn.java
+++ app/src/main/java/spe/uoblibraryapp/ActivitySignIn.java
@@ -1,13 +1,11 @@
 package spe.uoblibraryapp;
 
-import android.app.Activity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
