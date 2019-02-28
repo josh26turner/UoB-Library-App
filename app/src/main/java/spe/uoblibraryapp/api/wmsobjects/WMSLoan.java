2ead001a280548022f37f4add23dcb82b270bbdc
Denisi Murataj <denisimurataj@hotmail.co.uk>
2019-02-28 14:58:53 +0000

    removed un-used import

--- app/src/main/java/spe/uoblibraryapp/api/wmsobjects/WMSLoan.java
+++ app/src/main/java/spe/uoblibraryapp/api/wmsobjects/WMSLoan.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
