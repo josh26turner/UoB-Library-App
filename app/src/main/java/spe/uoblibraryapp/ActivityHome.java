50f6447c047f79e9cff58c2a2efd9e37fccd572e
Denisi Murataj <denisimurataj@hotmail.co.uk>
2019-02-28 11:56:07 +0000

    Removed un-necessary imports

--- app/src/main/java/spe/uoblibraryapp/ActivityHome.java
+++ app/src/main/java/spe/uoblibraryapp/ActivityHome.java
@@ -13,7 +13,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import spe.uoblibraryapp.api.AuthService;
 import spe.uoblibraryapp.api.ncip.WMSNCIPService;
