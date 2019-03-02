bf1d2e0e1faf5964ea0dcbd50f77bfe802efc65c
Josh Turner <joshaturner1@googlemail.com>
2019-03-02 15:25:36 +0000

    Removed unused import

--- app/src/main/java/spe/uoblibraryapp/ActivityScanNFC.java
+++ app/src/main/java/spe/uoblibraryapp/ActivityScanNFC.java
@@ -11,7 +11,6 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.NfcV;
 import android.os.Bundle;
-import android.os.Looper;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
